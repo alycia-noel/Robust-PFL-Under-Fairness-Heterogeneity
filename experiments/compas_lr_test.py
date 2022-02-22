9895167cd12ed1a9abf4eaf96d65023c031dab34
alycia <alycianoelcarey@gmail.com>
2022-02-22 09:06:50 -0600

    compas lr

---
+++ experiments/compas_lr_test.py
@@ -0,0 +1,157 @@
+import time
+import warnings
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from sklearn.metrics import roc_curve, auc
+
+warnings.filterwarnings("ignore")
+
+class TabularData(Dataset):
+    def __init__(self, X, y):
+        assert len(X) == len(y)
+        n, m = X.shape
+        self.n = n
+        self.m = m
+        self.X = torch.tensor(X, dtype=torch.float64)
+        self.y = torch.tensor(y, dtype=torch.float64)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+class LR(nn.Module):
+    def __init__(self, input_size):
+        super(LR, self).__init__()
+        self.fc1 = nn.Linear(input_size, 1)
+
+    def forward(self, x):
+        #x_flat = x.flatten()
+        y = self.fc1(x)
+        out = torch.sigmoid(y)
+
+        return out
+
+
+def plot_roc_curves(results, pred_col, resp_col, size=(7, 5), fname=None):
+    plt.clf()
+    plt.style.use('classic')
+    plt.figure(figsize=size)
+
+    for _, res in results.groupby('round'):
+        fpr, tpr, _ = roc_curve(res[resp_col], res[pred_col])
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, '-', color='orange', lw=0.5)
+
+    fpr, tpr, _ = roc_curve(results[resp_col], results[pred_col])
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, '-', color='darkorange', lw=1.5, label='ROC curve (area = %0.2f)' % roc_auc, )
+    plt.plot([0, 1], [0, 1], color='navy', lw=1.5, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.grid()
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend(loc="lower right")
+    #if fname is not None:
+    #    plt.savefig(fname)
+    #else:
+    plt.show()
+
+# Import the data and visualize it (if you want using df.info())
+# decile_score = risk score prediction
+url = 'https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv'
+df = pd.read_csv(url)
+
+# Cleaning and parsing the data
+# 1. If the charge date of a defendants COMPAS score was not within 30 days from when the person was arrested, we assume that because of data
+#    quality reason, that we do not have the right offense
+# 2. If is_recid = -1 then there was no COMPAS case found
+# 3. c_charge_degree of 'O' will result in no jail time so they are removed
+df_filtered = df.loc[df['days_b_screening_arrest'] <= 30]
+df_filtered = df_filtered.loc[df_filtered['days_b_screening_arrest'] >= -30]
+df_filtered = df_filtered.loc[df_filtered['is_recid'] != -1]
+df_filtered = df_filtered.loc[df_filtered['c_charge_degree'] != "O"]
+df_filtered = df_filtered.loc[df_filtered['score_text'] != 'N/A']
+df_filtered['is_med_or_high_risk']  = (df_filtered['decile_score']>=5).astype(int)
+df_filtered['length_of_stay'] = (pd.to_datetime(df_filtered['c_jail_out']) - pd.to_datetime(df_filtered['c_jail_in']))
+
+cols = ['sex', 'age', 'race', 'decile_score', 'length_of_stay', 'priors_count', 'c_charge_degree', 'two_year_recid', 'is_med_or_high_risk']
+compas = df_filtered[cols]
+compas['length_of_stay'] /= np.timedelta64(1, 'D')
+compas['length_of_stay'] = np.ceil(compas['length_of_stay'])
+
+cols = compas.columns
+features, decision = cols[:-1], cols[-1]
+
+encoders = {}
+for col in ['race', 'sex', 'c_charge_degree']:
+    encoders[col] = LabelEncoder().fit(compas[col])
+    compas.loc[:, col] = encoders[col].transform(compas[col])
+
+results = []
+
+
+d_train, d_test = train_test_split(compas, test_size=500)
+data_train = TabularData(d_train[features].values, d_train[decision].values)
+data_test = TabularData(d_test[features].values, d_test[decision].values)
+
+model = LR(input_size=8)
+model = model.double()
+
+train_loader = DataLoader(data_train, shuffle = True, batch_size = 16)
+test_loader = DataLoader(data_test, shuffle = False, batch_size= 16)
+
+optimizer = torch.optim.Adam(model.parameters(), lr = 2.e-4, weight_decay = 0.)
+loss = nn.MSELoss()
+no_batches = len(train_loader)
+
+# Train model
+for epoch in range(250):
+    start = time.time()
+    for i, (x, y) in enumerate(train_loader):
+        optimizer.zero_grad()
+        y_ = model(x)
+        err = loss(y_.flatten(), y)
+        err = err.mean()
+        err.backward()
+        optimizer.step()
+
+        #if i % 354 == 0:
+    print('Epoch: {0}/{1};\t Err: {2:1.3f}'.format(epoch + 1, 250, err.item()))
+    #\t Batch: {2}/{3};\t Err: {4:1.3f}'.format(epoch + 1, 250, i + 1, no_batches, err.item()))
+
+    #print('\n\t Epoch finished in {:1.2f} seconds!\n'.format(time.time() - start))
+
+# Eval Model
+    model.eval()
+    with torch.no_grad():
+        y_ = model(data_test.X)
+        #y_, p_ = model(data_test.X)
+        y_ = y_.flatten().numpy()
+        #p_ = p_.numpy()
+
+    res = (
+        pd  .DataFrame(columns = features, index = d_test.index)
+            .add_suffix('_partial')
+            .join(d_test)
+            .assign(prediction=y_)
+            .assign(round=i)
+    )
+
+    results.append(res)
+
+results = pd.concat(results)
+for col, encoder in encoders.items():
+        results.loc[:,col] = encoder.inverse_transform(results[col])
+
+
+plot_roc_curves(results, 'prediction', 'two_year_recid', size=(5, 3), fname='./results/roc.png')
