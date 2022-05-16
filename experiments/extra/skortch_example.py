9d7d7293facaa6219102a38ba7d1278d75338e72
Alycia <alycianoelcarey@gmail.com>
2022-05-16 12:07:19 -0500

    5/16/22 update

--- experiments/extra/skortch_example.py
+++ experiments/extra/skortch_example.py
@@ -8,7 +8,8 @@ import numpy as np
 import pandas as pd
 from collections import OrderedDict
 from skorch import NeuralNetClassifier
-from fairlearn.reductions import ExponentiatedGradient, DemographicParity, EqualizedOdds
+from fairlearn.reductions import DemographicParity, EqualizedOdds
+from experiments.new.fairlearn_modified.exponentiated_gradient import ExponentiatedGradient
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
 from sklearn.preprocessing import LabelEncoder
@@ -26,16 +27,32 @@ class LR(nn.Module):
             nn.Sigmoid()
         )
 
+        # self.context = nn.Sequential(
+        #     nn.Linear(self.input_size, 25),
+        #     nn.BatchNorm1d(25),
+        #     nn.ReLU(),
+        #     nn.Linear(25, 25),
+        #     nn.BatchNorm1d(25),
+        #     nn.ReLU(),
+        #     nn.Linear(25, self.input_size)
+        # )
+
     def forward(self, X, **kwargs):
         # this is where we can call the context vector
         # i.e., get context, concat with x, then call self.model(x)
+        # context_vector = self.context(X)
+        # avg_context_vector = torch.mean(context_vector, dim=0)
+        # prediction_vector = avg_context_vector.expand(len(X), len(X))
+        # pred_vec = torch.cat((prediction_vector, X), dim=1)
+
         return self.model(X)
 
 
 class SampleWeightLR(NeuralNetClassifier):
     def __init__(self, *args, criterion__reduce=False, **kwargs):
         super().__init__(*args, criterion__reduce=criterion__reduce, **kwargs)
-
+        # can we make a self.constraints = {}  and for each model fit store the context vectors and when we recall the best
+        # model also get the best constraint vectors so we can agg and send to the hypernework?
 
     #need to alter fit loop here to get collection of context vectors and we can update the hnet here as well
     # need run single epoch
@@ -57,14 +74,19 @@ class SampleWeightLR(NeuralNetClassifier):
         )
         X = {"X": X, "sample_weight": sample_weight}
 
-        return super().fit(X, y)
+        return super().fit(X, y) #this super().fit refers to the skorch fit method. This fit method is called during the fit (best_h) of expograd
 
     def predict(self, X):
+        # we can totally override this to be whatever we want!
+        # i.e., just use predict function from trainer code
+        # just need to check the other end for what is being returned (i.e., vector, matrix, dataframe ...)
         #here we need to get updated network param from HNET using context
         # 1. get context
         # 2. get param from hnet
         # 3. instantiate params
         # 4. classify
+
+        # will most likely just need to copy the predict_proba class from sklearn and modify
         if isinstance(X, (pd.DataFrame, pd.Series)):
             X = X.to_numpy().astype("float32")
         return(super().predict_proba(X) > 0.5).astype(float)
@@ -228,14 +250,28 @@ features_xa, features_x, label = cols[:-1], cols[:-2], cols[-1]
 all_client_train, all_client_test = all_client_test_train
 
 for j in range(num_clients):
+    # can we go ahead and get the context vector for the full dataset her? then just pass x + context in the fit?
+    # would we benefit from the learning process? - No, I don't think so. We would only be able to update the context network at
+    # the end of training the main network when it should be updated iteratively alongside it.
+        # get batch
+        # get context
+        # classify
+        # loss
+        # backprop
+        # repeat
+    # can we just copy the code for exponentiated gradient and modify it? I think this may be the best bet rather than trying to update
+    # sklearn fit
     print("Training client:", j+1)
     net = SampleWeightLR(LR(len(features_xa)), max_epochs=25 , optimizer=optim.Adam, lr=.001, batch_size=32, train_split=None, iterator_train__shuffle=True, criterion=nn.BCELoss, device=device)
     expgrad_xa = ExponentiatedGradient(net, constraints=DemographicParity(difference_bound=.01), eps=0.05, nu=1e-6)
 
+    # this calls the fit function of exponentiated gradient class
     expgrad_xa.fit(all_client_train[j][features_xa], all_client_train[j][label], sensitive_features=all_client_train[j]['sex'])
 
+    # this calls the predict function of the exponentatied gradient class
     scores_expgrad_xa = pd.Series(expgrad_xa.predict(all_client_test[j][features_xa]), name="scores_expgrad_xa")
 
+    # need to figure out how to use context here
     metric_frame_auc = MetricFrame(metrics=roc_auc_score, y_true=all_client_test[j][label], y_pred=scores_expgrad_xa, sensitive_features=all_client_test[j]['sex'])
     metric_frame_mean = MetricFrame(metrics=mean_prediction, y_true=all_client_test[j][label], y_pred=scores_expgrad_xa, sensitive_features=all_client_test[j]['sex'])
     auc = summary_as_df("auc", metric_frame_auc)
